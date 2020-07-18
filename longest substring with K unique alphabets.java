7da291ea4d95c05d2261635f7b2aac1bafcebc7d
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-07-18 13:39:22 +0530

    DT

--- longest substring with K unique alphabets.java
+++ longest substring with K unique alphabets.java
@@ -11,16 +11,35 @@ input: aaalllaAAAlll
 3
 output: 13
 */
-
 import java.util.*;
-import java.util.HashSet;
-import java.util.Set;
+public class Hello {
 
+    public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String s=sc.next();
+	int n=sc.nextInt(),max=0,len=s.length();
+	for(int i=0;i<len;i++){
+	    for(int j=i+1;j<=len;j++){
+	    String res=s.substring(i,j);
+	    Set<Character> set=new HashSet<>();
+	    for(char ch:res.toCharArray())
+	    set.add(ch);
+	    if(set.size()==n){
+	        max=Math.max(max,res.length());
+	    }
+	    }
+	}System.out.print(max);
+
+	}
+}
+		(or)
+		
+import java.util.*;
 class Main
 {
 	public static String lonSubstr(String a, int k)
 	{
-		int end = 0, begin = 0;
+		int end=0, begin=0;
 		Set<Character> s= new HashSet<>();
 		int[]  b= new int[126];
 		for (int i=0, j=0; j<a.length();j++)

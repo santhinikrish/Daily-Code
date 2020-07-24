7ea7450e098080638a895450592e722b1ba0793e
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-07-24 11:12:28 +0530

    DT

--- missing digit-divisible by 8.java
+++ missing digit-divisible by 8.java
@@ -46,3 +46,28 @@ public class Hellp {
         }
 	}
 }
+
+    (or)
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int tried = str.charAt(0)=='X'?1:0;
+		int ind = str.indexOf("X");
+		String fir = str.substring(0,ind);
+		String las = str.substring(ind+1);
+		while(tried!=10)
+		{
+		    if (Integer.valueOf(fir+tried+las)%8==0)
+		    {
+		        System.out.print(tried);
+		        return;
+		    }
+		    tried++;
+		}
+        System.out.print(-1);
+	}
+}

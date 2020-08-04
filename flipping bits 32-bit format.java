56fbf30866c065dcd93af013b754d4fb7ee6623a
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-08-04 22:27:56 +0530

    DT

---
+++ flipping bits 32-bit format.java
@@ -0,0 +1,57 @@
+/*
+Example Input/Output 1:
+Input: 2
+399861 7
+Output:
+4294567434
+4294967288
+Explanation:
+The binary representation of 399861 in 32 bits is 00000000000001100001100111110101.
+After toggling the bits, it becomes 11111111111110011110011000001010.
+The decimal equivalent of the above binary representation is 4294567434. So it is printed.
+The binary representation of 7 in 32 bits is 00000000000000000000000000000111.
+After toggling the bits, it becomes 11111111111111111111111111111000.
+The decimal equivalent of the above binary representation is 4294967288.
+So it is printed.
+
+Example Input/Output 2:
+Input: 3
+52568330
+45
+82730988
+Output:
+4242398965
+4294967250
+4212236307
+*/
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        long a[]=new long[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextLong();
+            String s=Long.toBinaryString(a[i]);
+            int l=s.length();
+            String d="",ds="";
+            if(l<32){
+                for(int j=0;j<32-l;j++){
+                    d+='0';
+                }
+            }
+            d+=s;
+            for(int j=0;j<d.length();j++){
+                if(d.charAt(j)=='0'){
+                    ds+='1';
+                }else{
+                    ds+='0';
+                }
+            }
+            long ans=Long.parseLong(ds,2);
+            System.out.println(ans+" ");
+        }
+	}
+}

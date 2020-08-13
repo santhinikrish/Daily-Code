91eda90c4b94c939037eb31e6121c6aef257b070
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-08-13 11:11:28 +0530

    DT

--- concatenate four matrix pattern.java
+++ concatenate four matrix pattern.java
@@ -109,3 +109,66 @@ public class Main {
         }
 	}
 }
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n,i,j;
+   scanf("%d",&n);
+   int a[n][n];
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<n;j++)
+       {
+           scanf("%d",&a[i][j]);
+       }
+   }
+   int x=0;
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<n-x;j++)
+       {
+           printf("%d ",a[i][j]);
+       }
+       for(j=0;j<x;j++)
+       {
+           printf("* ");
+       }
+       for(j=0;j<x;j++)
+       {
+           printf("* ");
+       }
+       for(j=x;j<n;j++)
+       {
+           printf("%d ",a[i][j]);
+       }
+       x+=1;
+       printf("\n");
+   }
+   x=0;
+ //  int y=
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<=x;j++)
+       {
+           printf("%d ",a[i][j]);
+       }
+       for(j=0;j<n-x-1;j++)
+       {
+           printf("* ");
+       }
+       for(j=0;j<n-x-1;j++)
+       {
+           printf("* ");
+       }
+       for(j=n-x-1;j<n;j++)
+       {
+           printf("%d ",a[i][j]);
+       }
+       x+=1;
+       printf("\n");
+   }
+
+}

c8126a70fb2200b6632ae4bae188f267508f795a
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-08-21 22:56:18 +0530

    DC

--- palindrome-prev or next.java
+++ palindrome-prev or next.java
@@ -39,3 +39,26 @@ public class Hello {
         System.out.print("YES");
 	}
 }
+
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char[] str=sc.nextLine().toCharArray();
+		int len=str.length,flag=0;
+		for(int index=0;index<len/2;index++){
+		    char ch1=str[index],ch2=str[len-index-1];
+		    if(Math.abs(ch1-ch2)>2){
+		        System.out.print("NO");
+		        flag=1;
+		        break;
+		    }
+		}
+		if(flag==0){
+		    System.out.print("YES");
+		}
+
+	}
+}

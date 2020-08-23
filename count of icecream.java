d149d50d27d8ce43b8067e92b92b1bfacffaddb5
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-08-23 22:12:37 +0530

    DC

--- count of icecream.java
+++ count of icecream.java
@@ -23,7 +23,6 @@ Output: 6
 
 import java.util.*;
 public class Hello {
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt(),k=sc.nextInt();

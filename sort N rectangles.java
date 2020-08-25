b397a3d7ae06f5f197fa8606a013fe54094d1f87
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-08-25 23:02:14 +0530

    DC

--- sort N rectangles.java
+++ sort N rectangles.java
@@ -1,3 +1,47 @@
+/*
+Example Input/Output 1:
+Input: 5
+5 7
+2 5
+3 2
+2 3
+4 6
+Output:
+2 3 6
+3 2 6
+2 5 10
+4 6 24
+5 7 35
+Explanation:
+The area of the first rectangle is 35 (5*7).
+The area of the second rectangle is 10 (2*5).
+The area of the third rectangle is 6 (3*2).
+The area of the fourth rectangle is 6 (2*3).
+The area of the fifth rectangle is 24 (4*6).
+After sorting the rectangles based on the given conditions, the rectangles become
+2 3 6
+3 2 6
+2 5 10
+4 6 24
+5 7 35
+
+Example Input/Output 2:
+Input: 6
+20 5
+3 20
+15 4
+10 10
+11 2
+5 5
+Output:
+11 2 22
+5 5 25
+3 20 60
+15 4 60
+10 10 100
+20 5 100
+*/
+
 import java.util.*;
 public class Hello {
 
